7dc143f137ed4112b66304877ff7ba2cd3b85a7c
AntoninRousset <AntoninRousset@users.noreply.github.com>
2019-11-28 22:22:11 +0100

    correct name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='escaperoom-display',
+    name='escaperoom_display',
     version='0.1a1',
     license='GPL-3',
     author='Antonin Rousset',
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/pypa/sampleproject',
-    py_modules=['escaperoom-display'],
+    py_modules=['escaperoom_display'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
